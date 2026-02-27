80cb2d0309684075c78c5c1205ba21110f8cce6a
paperthink <pattaboissxp2010@gmail.com>
2026-02-27 23:12:26 +0530

    success

---
+++ extra_secure.py
@@ -0,0 +1,11 @@
+#DISCLAIMER: DO NOT PUT YOUR BANK ID. 
+# #though its not possible to also hhheheh
+user = str(input('Enter ur ID(Any ID) : '))
+l = len(user)
+#for h in user:
+if l >=2:
+    u = "x" * (l-2)
+
+print( )
+
+
---
+++ f.py
@@ -0,0 +1,7 @@
+#this code is to find out the length
+
+user = str(input("Enter some text: "))
+
+words = user.split()
+
+print(len(words[-1]))
