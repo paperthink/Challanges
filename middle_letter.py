73adf1996fa8114cdb0708b5e24266f7770150ca
paperthink <pattaboissxp2010@gmail.com>
2026-02-28 21:38:05 +0530

    reduced_miscllaneous

--- middle_letter.py
+++ middle_letter.py
@@ -5,7 +5,8 @@ def get_middle(s):
         can_middle = True
         if can_middle ==True:
             for a in range(size):
-                if a == size//2 + 1//2:#median formula(for odd)
+                if a == size//2:#median formula(for odd)
+                    #we know that indices count from 0 so anything/2 is always +1 from our expectations
                     print(s[a])
                     break
     if size % 2==0:
@@ -13,4 +14,4 @@ def get_middle(s):
         if can_middle == False:
             print(s[size//2 - 1]+s[size//2])
 
-get_middle("Absquatulate")
+get_middle(input("Enter any String: "))
