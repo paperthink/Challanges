03225d97bd532b09c5acd672d89ec9df1276c72b
paperthink <pattaboissxp2010@gmail.com>
2026-03-26 11:18:37 +0530

    ongoing

--- reverse.py
+++ reverse.py
@@ -2,6 +2,6 @@ def reverse(input_text):
     for letter in input_text:
         input_text = letter + input_text
 
-    print(input_text)
+    return input_text
 
 a = reverse(input("Enter any word: "))
